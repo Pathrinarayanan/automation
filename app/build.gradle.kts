b71547bd9685e99c87031ebc26a852acdb3f3e7d
Narayanan <narayanan@CLLAP-CHN-2576.local>
2024-10-28 10:45:03 +0530

    added debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     }
 
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
